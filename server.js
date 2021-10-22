dd2916c41d0c4f9a2332040f54701ee095cb0e15
Fatoumata86 <soukounafatoumata86@gmail.com>
2021-10-22 15:55:22 +0200

    trippy projets part 1

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const exphbs = require("express-handlebars");
+
+const port = 8000;
+
+app.use(express.json());
+
+app.engine('hbs', exphbs());
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'));
+
+const hotels = [
+    { name: "Hilton"},
+    { name: "Ritz"},
+    { name: "Trianon Palace"},
+]
+
+app.get('/hotels', (req, res) => {
+    // console.log('Liste des Hotels : ', hotels);
+    res.send(hotels);
+});
+
+app.get('/hotels/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log("L'hotel rechercher est : ", `${hotels[id - 1].name}`);
+    res.send(`${hotels[id - 1].name}`);
+});
+
+app.post('/hotels', (req, res) => {
+    const hotel = req.body;
+// console.log(hotel);
+    hotels.push(hotel);
+    // console.log("L'hotel ajouter est : ", hotels);
+    res.json(hotels);
+});
+
+app.put('/hotels/:id?name', (req, res) => {
+    const updatingHotelId = req.params.id;
+    const newname = req.query.name;
+    hotels[updatingHotelId].name = newname;
+    console.log("L'hotel mise à jour est : ", newname);
+    res.json(hotels);
+});
+
+app.listen(port, () => {
+    console.log(`The server is started on port : ${port}`);
+});
